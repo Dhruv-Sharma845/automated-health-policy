2de81d125ee1e6a67cf82fac210ae5fcc8bc288e
dhruvsharma <dhruv.sharma@accoliteindia.com>
2018-02-26 14:23:37 +0530

    resolved Jpa issue

--- src/main/java/com/angularjsspring/crud/CrudApplication.java
+++ src/main/java/com/angularjsspring/crud/CrudApplication.java
@@ -2,7 +2,11 @@ package com.angularjsspring.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.angularjsspring.crud.config.JpaConfig;
+
+@Import(JpaConfig.class)
 @SpringBootApplication
 public class CrudApplication {
 
