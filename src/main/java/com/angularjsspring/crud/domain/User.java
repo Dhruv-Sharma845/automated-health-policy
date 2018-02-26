eb6d6eae62642d55d83df4db9e86b71162ea74ac
dhruvsharma <dhruv.sharma@accoliteindia.com>
2018-02-26 16:51:45 +0530

    added Service and Controller classes

--- src/main/java/com/angularjsspring/crud/domain/User.java
+++ src/main/java/com/angularjsspring/crud/domain/User.java
@@ -1,5 +1,7 @@
 package com.angularjsspring.crud.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,7 +13,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name="user")
-public class User {
+public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +23,25 @@ public class User {
 	@Column(name="name", nullable=false)
 	private String name;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+	
 }
